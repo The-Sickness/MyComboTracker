7cbc46d65f56123244fa45385e9f7a89908d7e95
The-Sickness <alloycowboy6969@gmail.com>
2023-05-15 01:48:53 -0500

    Fixed combo point issues

--- MyComboTracker.lua
+++ MyComboTracker.lua
@@ -1,6 +1,6 @@
 -- My Combo Tracker
 -- Made by Sharpedge_Gaming
--- v1.1	 - 10.1
+-- v1.1 - 10.1
 
 local addonName, addonTable = ...
 local AceAddon = LibStub("AceAddon-3.0")
@@ -33,6 +33,22 @@ fade:SetStartDelay(1)
 
 ag:SetScript("OnFinished", function() text:Hide() end)
 
+local function PlaySoundAtComboPoints(comboPoints)
+    local specialization = GetSpecialization()
+    local selectedSound = MyComboTrackerSettings.selectedSound
+
+    if (specialization == 1 and comboPoints == 5) or ((specialization == 2 or specialization == 3) and comboPoints == 6) then
+        if selectedSound then
+            local soundFile = MyComboTrackerSettings.soundOptions[selectedSound]
+            if soundFile then
+                PlaySoundFile(soundFile, "Master")
+            end
+        end
+    end
+end
+
+
+
 local function UpdateComboPoints()
     local comboPoints = UnitPower("player", Enum.PowerType.ComboPoints)
     if comboPoints and comboPoints > 0 then
@@ -43,18 +59,46 @@ local function UpdateComboPoints()
         move:SetOffset(0, 200)
         ag:Play()
 
-        if comboPoints == 5 then
-            local selectedSound = MyComboTrackerSettings.selectedSound
-            if selectedSound then
-                local soundFile = MyComboTrackerSettings.soundOptions[selectedSound]
-                PlaySoundFile(soundFile)
+        PlaySoundAtComboPoints(comboPoints)
+    end
+end
+
+local currentSpec = -1
+
+local function CheckSpec()
+    local spec = GetSpecialization()
+    if spec ~= currentSpec then
+        currentSpec = spec
+        if currentSpec == 2 then -- Subtlety Rogue
+            local comboPoints = UnitPower("player", Enum.PowerType.ComboPoints)
+            if comboPoints >= 6 then
+                local selectedSound = MyComboTrackerSettings.selectedSound .. "_6"
+                if selectedSound then
+                    local soundFile = MyComboTrackerSettings.soundOptions[selectedSound]
+                    if soundFile then
+                        PlaySoundFile(soundFile, "Master")
+                    end
+                end
+            end
+        elseif currentSpec == 1 then -- Assassin Rogue
+            local comboPoints = UnitPower("player", Enum.PowerType.ComboPoints)
+            if comboPoints == 5 then
+                local selectedSound = MyComboTrackerSettings.selectedSound
+                if selectedSound then
+                    local soundFile = MyComboTrackerSettings.soundOptions[selectedSound]
+                    if soundFile then
+                        PlaySoundFile(soundFile, "Master")
+                    end
+                end
             end
         end
     end
 end
 
+
 frame:RegisterEvent("UNIT_POWER_UPDATE")
 frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "UNIT_POWER_UPDATE" then
         local unit, powerType = ...
@@ -71,6 +115,8 @@ frame:SetScript("OnEvent", function(self, event, ...)
         if not MyComboTracker.initialized then
             MyComboTracker:OnInitialize()
         end
+    elseif event == "PLAYER_SPECIALIZATION_CHANGED" then
+        CheckSpec()
     end
 end)
 
@@ -82,19 +128,29 @@ function MyComboTracker:OnInitialize()
     MyComboTrackerSettings = SV.profile
 
     MyComboTrackerSettings.soundOptions = {
-        ["Sound1"] = "Interface\\Addons\\MyComboTracker\\sounds\\ReadyCheck.mp3",
-        ["Sound2"] = "Interface\\Addons\\MyComboTracker\\sounds\\AxeCrit.mp3",
-        ["Sound3"] = "Interface\\Addons\\MyComboTracker\\sounds\\Parry.mp3",
-        ["Sound4"] = "Interface\\Addons\\MyComboTracker\\sounds\\SealofMight.mp3",
-		["Sound5"] = "Interface\\Addons\\MyComboTracker\\sounds\\Sheldon.mp3",
-    }
+            ["Sound1"] = "Interface\\Addons\\MyComboTracker\\sounds\\ReadyCheck.mp3",
+            ["Sound2"] = "Interface\\Addons\\MyComboTracker\\sounds\\AxeCrit.mp3",
+            ["Sound3"] = "Interface\\Addons\\MyComboTracker\\sounds\\Parry.mp3",
+            ["Sound4"] = "Interface\\Addons\\MyComboTracker\\sounds\\SealofMight.mp3",
+            ["Sound5"] = "Interface\\Addons\\MyComboTracker\\sounds\\Sheldon.mp3",
+            ["Sound6"] = "Interface\\Addons\\MyComboTracker\\sounds\\Arrow Swoosh.mp3",
+            ["Sound7"] = "Interface\\Addons\\MyComboTracker\\sounds\\Buzzer.mp3",
+            ["Sound8"] = "Interface\\Addons\\MyComboTracker\\sounds\\Gun Cocking.mp3",
+            ["Sound9"] = "Interface\\Addons\\MyComboTracker\\sounds\\Laser.mp3",
+            ["Sound10"] = "Interface\\Addons\\MyComboTracker\\sounds\\Target Acquired.mp3",
+        }
 
-    local soundOptionNames = {
-        ["Sound1"] = "Ready Check",
-        ["Sound2"] = "Axe Crit",
-        ["Sound3"] = "Parry",
-        ["Sound4"] = "Seal of Might",
-        ["Sound5"] = "Sheldon",
+        local soundOptionNames = {
+            ["Sound1"] = "Ready Check",
+            ["Sound2"] = "Axe Crit",
+            ["Sound3"] = "Parry",
+            ["Sound4"] = "Seal of Might",
+            ["Sound5"] = "Sheldon",
+            ["Sound6"] = "Arrow Swoosh",
+            ["Sound7"] = "Buzzer",
+            ["Sound8"] = "Gun Cocking",
+            ["Sound9"] = "Laser",
+            ["Sound10"] = "Target Acquired",
     }
 
     local options = {
@@ -111,7 +167,7 @@ function MyComboTracker:OnInitialize()
                 get = function(info) return MyComboTrackerSettings.textSize end,
                 set = function(info, value)
                     MyComboTrackerSettings.textSize = value
-                    text:SetFont("Fonts\\FRIZQT__.TTF", value, "OUTLINE")
+                    text:SetFont(LSM:Fetch("font", MyComboTrackerSettings.selectedFont), value, "OUTLINE")
                 end,
                 order = 1,
             },
@@ -123,15 +179,26 @@ function MyComboTracker:OnInitialize()
                 get = function(info) return MyComboTrackerSettings.selectedSound end,
                 set = function(info, value)
                     MyComboTrackerSettings.selectedSound = value
+                    local soundFile = MyComboTrackerSettings.soundOptions[value]
+                    if soundFile then
+                        PlaySoundFile(soundFile, "Master")
+                    end
                 end,
                 order = 2,
-            },
+            
+           },
+
         },
     }
-
+	
+	
+	
+	
     AceConfig:RegisterOptionsTable(addonName, options)
     AceConfigDialog:AddToBlizOptions(addonName, addonName)
 end
+
+
 
 
 
